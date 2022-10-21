857ddbf0096bca96d0d051ce9a38a02c580ff9b6
Abdullah Al Mahdi <61989438+Mahdi-Abdullah@users.noreply.github.com>
2022-10-21 19:02:34 +0600

    data and course class creation

---
+++ python_project/course.py
@@ -0,0 +1,23 @@
+'''Course Management Application'''
+
+class Course:
+    '''A class containing course functionalitis'''
+
+    def __init__(self, course_code, course_title, course_credit, prerequisites):
+        '''Initialize name and age attributes'''
+        self.course_code = course_code
+        self.course_title = course_title
+        self.course_credit = course_credit
+        self.prerequisites = prerequisites
+
+    def all_course_display(self):
+        '''To display information about all the courses'''
+        pass
+
+    def adding_new_course(self):
+        '''To add a new course'''
+        pass
+
+    def updating_existing_course(self):
+        ''''''
+        pass
